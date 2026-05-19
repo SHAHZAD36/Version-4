b35c6d91146e7fc265c9134ff92fa5c6278a8df7
SHAHZAD36 <vengeance83836@gmail.com>
2026-05-20 01:00:57 +0500

    Update sale_provider.dart

--- lib/features/sales/presentation/providers/sale_provider.dart
+++ lib/features/sales/presentation/providers/sale_provider.dart
@@ -1,43 +1,36 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../../core/utils/database_helper.dart';
 import '../../data/models/sale_model.dart';
-import '../../data/repositories/sale_repository_impl.dart';
 import '../../domain/repositories/sale_repository.dart';
-import '../../../products/presentation/providers/product_provider.dart';
-import '../../../customers/presentation/providers/customer_provider.dart';
+import '../../data/repositories/sale_repository_impl.dart';
+import '../../../../core/utils/database_helper.dart';
 
 final saleRepositoryProvider = Provider<SaleRepository>((ref) {
   return SaleRepositoryImpl(DatabaseHelper.instance);
 });
 
 final salesProvider = StateNotifierProvider<SaleNotifier, List<SaleModel>>((ref) {
-  return SaleNotifier(ref.watch(saleRepositoryProvider), ref);
+  return SaleNotifier(ref.watch(saleRepositoryProvider));
 });
 
 class SaleNotifier extends StateNotifier<List<SaleModel>> {
   final SaleRepository _repository;
-  final Ref _ref;
 
-  SaleNotifier(this._repository, this._ref) : super([]) {
+  SaleNotifier(this._repository) : super([]) {
     loadSales();
-
-    // Add this inside the SaleNotifier class
-Future<void> deleteSale(int id) async {
-  final repository = ref.read(saleRepositoryProvider); // Make sure your repository provider name matches
-await _repository.deleteSale(id);
-state = await _repository.getSales();
-}
   }
 
   Future<void> loadSales() async {
     state = await _repository.getSales();
   }
 
+  // CORRECTED DELETE METHOD:
+  Future<void> deleteSale(int id) async {
+    await _repository.deleteSale(id); // Use _repository directly
+    await loadSales(); // Refresh the list
+  }
+
   Future<void> createSale(SaleModel sale, List<SaleItemModel> items) async {
     await _repository.createSale(sale, items);
     await loadSales();
-    // Refresh products and customers to reflect stock and balance changes
-    _ref.read(productsProvider.notifier).loadProducts();
-    _ref.read(customersProvider.notifier).loadCustomers();
   }
 }
