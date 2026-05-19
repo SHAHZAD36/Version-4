94f42dff9e1d6a24220e5a4ab7e07c6ea79bc170
SHAHZAD36 <vengeance83836@gmail.com>
2026-05-20 00:29:28 +0500

    Update sale_repository_impl.dart

--- lib/features/sales/data/repositories/sale_repository_impl.dart
+++ lib/features/sales/data/repositories/sale_repository_impl.dart
@@ -1,4 +1,18 @@
 // Fetching only today's sales
+class SaleRepositoryImpl implements SaleRepository {
+  // 1. ADD THIS LINE if it is missing
+  final DatabaseHelper _dbHelper; 
+
+  // 2. The constructor should look like this
+  SaleRepositoryImpl(this._dbHelper);
+
+  Future<List<SaleModel>> getSales() async {
+    // 3. Use the _dbHelper here
+    final db = await _dbHelper.database; 
+    // ... rest of the code ...
+  }
+}
+
 Future<List<SaleModel>> getTodaySales() async {
   final db = await databaseHelper.database;
   final String today = DateTime.now().toString().split(' ')[0]; // Gets "2023-10-27"
