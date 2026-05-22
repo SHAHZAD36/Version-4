ec13d82e768ec55e956f3f3758cd115c81a09704
SHAHZAD36 <vengeance83836@gmail.com>
2026-05-22 16:00:30 +0500

    Update collection_provider.dart

--- lib/features/collections/presentation/providers/collection_provider.dart
+++ lib/features/collections/presentation/providers/collection_provider.dart
@@ -1,24 +1,64 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite/sqflite.dart';
 import '../../../../core/utils/database_helper.dart';
 import '../../data/models/collection_model.dart';
-import '../../data/repositories/collection_repository_impl.dart';
-import '../../../customers/presentation/providers/customer_provider.dart';
-import '../../../dashboard/presentation/providers/dashboard_provider.dart';
-
-final collectionRepoProvider = Provider((_) => CollectionRepositoryImpl(DatabaseHelper.instance));
-
-final collectionsProvider = StateNotifierProvider<CollectionNotifier, List<CollectionModel>>(
-    (ref) => CollectionNotifier(ref.watch(collectionRepoProvider), ref));
 
 class CollectionNotifier extends StateNotifier<List<CollectionModel>> {
-  final CollectionRepositoryImpl _r;
-  final Ref _ref;
-  CollectionNotifier(this._r, this._ref) : super([]) { load(); }
-  Future<void> load() async { state = await _r.getCollections(); }
-  Future<void> add(CollectionModel c) async {
-    await _r.addCollection(c);
-    await load();
-    _ref.read(customersProvider.notifier).loadCustomers();
-    _ref.read(dashboardProvider.notifier).loadStats();
+  CollectionNotifier() : super([]) {
+    loadCollections();
+  }
+
+  Future loadCollections() async {
+    try {
+      final db = await DatabaseHelper.instance.database;
+      final result = await db.query('collections', orderBy: 'id DESC');
+      state = result.map((e) => CollectionModel.fromMap(e)).toList();
+    } catch (e) {
+      state = [];
+    }
+  }
+
+  Future addCollection(CollectionModel collection) async {
+    try {
+      final db = await DatabaseHelper.instance.database;
+      final id = await db.insert('collections', collection.toMap());
+      
+      // Update customer balance
+      await db.rawUpdate(
+        'UPDATE customers SET current_balance = current_balance - ? WHERE id = ?',
+        [collection.amount, collection.customerId]
+      );
+      
+      await loadCollections();
+    } catch (e) {
+      debugPrint('Error adding collection: $e');
+    }
+  }
+
+  Future deleteCollection(int id) async {
+    try {
+      final db = await DatabaseHelper.instance.database;
+      final collection = await db.query('collections', where: 'id = ?', whereArgs: [id]);
+      
+      if (collection.isNotEmpty) {
+        final amount = collection.first['amount'] as double;
+        final customerId = collection.first['customer_id'] as int;
+        
+        // Restore customer balance
+        await db.rawUpdate(
+          'UPDATE customers SET current_balance = current_balance + ? WHERE id = ?',
+          [amount, customerId]
+        );
+      }
+      
+      await db.delete('collections', where: 'id = ?', whereArgs: [id]);
+      await loadCollections();
+    } catch (e) {
+      debugPrint('Error deleting collection: $e');
+    }
   }
 }
+
+final collectionsProvider = StateNotifierProvider<CollectionNotifier, List<CollectionModel>>((ref) {
+  return CollectionNotifier();
+});
