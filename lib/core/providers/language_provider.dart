daf45967b4f01f01998ff0619093aef82bcb2109
SHAHZAD36 <vengeance83836@gmail.com>
2026-05-22 19:43:20 +0500

    Update language_provider.dart

--- lib/core/providers/language_provider.dart
+++ lib/core/providers/language_provider.dart
@@ -1,31 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 enum AppLanguage { english, urdu }
 
 class LanguageNotifier extends StateNotifier<AppLanguage> {
-  LanguageNotifier() : super(AppLanguage.urdu) {
-    _loadLanguage();
-  }
-
-  Future<void> _loadLanguage() async {
-    final prefs = await SharedPreferences.getInstance();
-    final langIndex = prefs.getInt('app_lang_index') ?? 1;
-    state = AppLanguage.values[langIndex];
-  }
-
-  Future<void> setLanguage(AppLanguage lang) async {
-    state = lang;
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt('app_lang_index', lang.index);
-  }
+  LanguageNotifier() : super(AppLanguage.urdu);
 
   void toggleLanguage() {
-    if (state == AppLanguage.urdu) {
-      setLanguage(AppLanguage.english);
-    } else {
-      setLanguage(AppLanguage.urdu);
-    }
+    state = state == AppLanguage.urdu ? AppLanguage.english : AppLanguage.urdu;
+  }
+
+  void setLanguage(AppLanguage lang) {
+    state = lang;
   }
 }
 
