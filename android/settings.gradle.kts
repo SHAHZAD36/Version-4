70bc3acb697dc3b7a908ad5b4df3e64f2b255090
SHAHZAD36 <vengeance83836@gmail.com>
2026-05-23 16:33:18 +0500

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -6,9 +6,7 @@ pluginManagement {
         require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
     }
-
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
         google()
         mavenCentral()
@@ -17,9 +15,9 @@ pluginManagement {
 }
 
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
 
 include(":app")
